dfbb2ba7c09c9e24b2d31bf195603dfde724586b
KryunYT <101967367+KryunYT@users.noreply.github.com>
2025-05-23 10:50:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pypresence import Presence
 import time
 
-client_id = "1349759901957881907"  
+client_id = "11111111111111111111"  
 RPC = Presence(client_id)
 RPC.connect()  
 
@@ -9,7 +9,7 @@ RPC.connect()
 RPC.update(
     details="🤖 Coding right now",  
     state="In MinecraftCheat",    
-    large_image="ai_logo",          
+    large_image="alogo",          
     buttons=[{"label": "Donate", "url": "https://www.donationalerts.com/r/kryun"}]
 )
 
